e44b29ed48d323612688436b30e4b39ca1cce762
Yeasir <yeasir@cdda.io>
2022-06-29 00:21:19 +0600

    added type in button

--- src/components/CustomBtn.js
+++ src/components/CustomBtn.js
@@ -1,5 +1,5 @@
-import { Box, Button, Spinner } from '@chakra-ui/react';
-import { AiFillCheckCircle } from 'react-icons/ai';
+import { Box, Button, Spinner } from "@chakra-ui/react";
+import { AiFillCheckCircle } from "react-icons/ai";
 
 const CustomBtn = ({
   loading,
@@ -9,11 +9,12 @@ const CustomBtn = ({
   leftIcon,
   rightIcon,
   disabled,
+  type
 }) => {
   return (
     <>
       {/* normal button  */}
-      {typeof done === 'undefined' && (
+      {typeof done === "undefined" && (
         <Button
           disabled={disabled ? true : false}
           leftIcon={leftIcon ? leftIcon : false}
@@ -29,52 +30,51 @@ const CustomBtn = ({
                 size="lg"
               />
               <Box
-                position={'absolute'}
+                position={"absolute"}
                 top="12px"
-                left={'12px'}
+                left={"12px"}
                 as="span"
-                h={'8px'}
-                w={'8px'}
+                h={"8px"}
+                w={"8px"}
                 bg="white"
               ></Box>
             </>
           }
           _disabled={{
-            backgroundColor: '#6F798B',
-            opacity: '.6',
-            cursor: 'not-allowed',
+            backgroundColor: "#6F798B",
+            opacity: ".6",
+            cursor: "not-allowed",
           }}
           textTransform="capitalize"
-          loadingText={
-            typeof loadingText === 'string' ? loadingText : 'Submitting'
-          }
+          loadingText={typeof loadingText === 'string' ? loadingText : 'Submitting'}
           fontWeight="normal"
           height="40px"
           width="117px"
           fontSize="14px"
           color="white"
           bgColor="#00885C"
-          _hover={{ backgroundColor: '#005B3E' }}
-          _active={{ backgroundColor: '#21D233' }}
+          _hover={{ backgroundColor: "#005B3E" }}
+          _active={{ backgroundColor: "#21D233" }}
+          type={typeof type === 'string' ? type : 'button'}
         >
-          {typeof text === 'string' ? text : 'Submit'}
+          {typeof text === "string" ? text : "Submit"}
         </Button>
       )}
       {/* done button  */}
       {done && (
         <Button
           textTransform="capitalize"
-          leftIcon={<AiFillCheckCircle fontSize={'30px'} />}
+          leftIcon={<AiFillCheckCircle fontSize={"30px"} />}
           fontWeight="normal"
           height="45px"
           width="134px"
           fontSize="15px"
           color="white"
           bgColor="#8EC03F"
-          _hover={{ backgroundColor: '#8EC03F' }}
-          _active={{ backgroundColor: '#577823' }}
+          _hover={{ backgroundColor: "#8EC03F" }}
+          _active={{ backgroundColor: "#577823" }}
         >
-          {typeof text === 'string' ? text : 'Done'}
+          {typeof text === "string" ? text : "Done"}
         </Button>
       )}
     </>
